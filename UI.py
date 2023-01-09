78d7d553449539a0dac7d13b7473ea87d718b536
DilshodN <dilshod8471@gmail.com>
2023-01-09 16:36:46 +0700

    123

--- UI.py
+++ UI.py
@@ -1,59 +1,47 @@
-# tkinter
-import tkinter as tk
-from tkinter import filedialog
+# def importFile(self):
+#     filename = filedialog.askopenfilename()
+#     self.experimentalFreq, self.experimentalOpticalDensity = parser.parseASCII(filename)
+#
+#
+# def plotExperimentalData(self):
+#     fig = Figure(figsize=(7, 5), dpi=100)
+#
+#     plotter = fig.add_subplot(111)
+#     plotter.plot(self.experimentalFreq, self.experimentalOpticalDensity)
+#     plotter.set_xlabel(r'Frequency [sm^(-1)]')
+#     plotter.set_ylabel(r'Optical density')
+#
+#     canvas = FigureCanvasTkAgg(fig, master=self)
+#     canvas.draw()
+#     canvas.get_tk_widget().pack()
 
-# calculations & plot
-import matplotlib
-import numpy as np
-
-matplotlib.use('TkAgg')
-
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg,
-    NavigationToolbar2Tk
-)
-
-# Our files
-import parser
-
-
-class App(tk.Tk):
-    def __init__(self, height=800, width=600):
-        super().__init__()
-        # Tkinter
-        self.title("Optical Properties of Semiconductor")
-        self.geometry(f'{height}x{width}')
-
-        # self.canvas = tk.Canvas(self, width=width, height=height, bg='white')
-        # self.canvas.pack()
-        # Buttons
-        importFileButton = tk.Button(self, text='Import file', command=self.importFile)
-        importFileButton.pack()
-
-        drawExperimentalData = tk.Button(self, text='Plot data', command=self.plotExperimentalData)
-        drawExperimentalData.pack()
-        # Data
-        self.experimentalFreq = None
-        self.experimentalOpticalDensity = None
-
-    def importFile(self):
-        filename = filedialog.askopenfilename()
-        self.experimentalFreq, self.experimentalOpticalDensity = parser.parseASCII(filename)
-
-    def plotExperimentalData(self):
-        fig = Figure(figsize=(7, 5), dpi=100)
-
-        plotter = fig.add_subplot(111)
-        plotter.plot(self.experimentalFreq, self.experimentalOpticalDensity)
-        plotter.set_xlabel(r'Frequency [sm^(-1)]')
-        plotter.set_ylabel(r'Optical density')
-
-        canvas = FigureCanvasTkAgg(fig, master=self)
-        canvas.draw()
-        canvas.get_tk_widget().pack()
+from PyQt5 import QtWidgets, uic
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+import os
 
 
-app = App()
+class MainWindow(QtWidgets.QMainWindow):
 
-app.mainloop()
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi("../qtui/mainWindow.ui", self)
+
+        self.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [30, 32, 34, 32, 33, 31, 29, 32, 35, 45])
+
+    def plot(self, hour, temperature):
+        self.graphWidget.plot(hour, temperature)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
